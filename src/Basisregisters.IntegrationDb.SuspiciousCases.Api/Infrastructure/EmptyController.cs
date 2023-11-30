712e4248c3acd6addb5ddc3f48cc0a485d597adc
Arne Dumarey <dumarey.arne@gmail.com>
2023-11-30 19:04:56 +0100

    feat: add suspicious cases api skeleton

---
+++ src/Basisregisters.IntegrationDb.SuspiciousCases.Api/Infrastructure/EmptyController.cs
@@ -0,0 +1,19 @@
+namespace Basisregisters.IntegrationDb.SuspiciousCases.Api.Infrastructure
+{
+    using System.Reflection;
+    using Be.Vlaanderen.Basisregisters.Api;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Net.Http.Headers;
+
+    [ApiVersionNeutral]
+    [Route("")]
+    public class EmptyController : ApiController
+    {
+        [HttpGet]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult Get()
+            => Request.Headers[HeaderNames.Accept].ToString().Contains("text/html")
+                ? (IActionResult)new RedirectResult("/docs")
+                : new OkObjectResult($"Welcome to the Basisregisters Vlaanderen Verdachte Gevallen Api {Assembly.GetEntryAssembly().GetVersionText()}.");
+    }
+}
---
+++ src/Basisregisters.IntegrationDb.SuspiciousCases.Api/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Suspiciouse Cases API")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("c5b6b5e0-8b9a-4b9e-9b9a-9e9b9a9b9a9b")]
