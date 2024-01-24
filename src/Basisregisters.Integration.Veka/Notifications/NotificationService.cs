fa46c5a393da7c29fe05b826f2af359b104d55e8
emalfroy <egon.malfroy@gmail.com>
2024-01-24 11:57:40 +0100

    feat: add notification service

---
+++ src/Basisregisters.Integration.Veka/Notifications/NotificationService.cs
@@ -0,0 +1,52 @@
+﻿namespace Basisregisters.Integration.Veka.Notifications
+{
+    using System;
+    using System.Text.Json;
+    using System.Threading.Tasks;
+    using Amazon.SimpleNotificationService;
+    using Amazon.SimpleNotificationService.Model;
+
+    public interface INotificationService
+    {
+        Task PublishToTopicAsync(NotificationMessage message);
+    }
+
+    public class NotificationService : INotificationService, IDisposable
+    {
+        private readonly IAmazonSimpleNotificationService _amazonSimpleNotificationService;
+        private readonly string _topicArn;
+
+        public NotificationService(IAmazonSimpleNotificationService amazonSimpleNotificationService, string topicArn)
+        {
+            _amazonSimpleNotificationService = amazonSimpleNotificationService;
+            _topicArn = topicArn;
+        }
+
+        public async Task PublishToTopicAsync(NotificationMessage message)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            var request = new PublishRequest
+            {
+                TopicArn = _topicArn,
+                Message = JsonSerializer.Serialize(message, options),
+                MessageAttributes =
+                {
+                    { "MessageType", new MessageAttributeValue { DataType = "String", StringValue = message.MessageType } },
+                    { "service", new MessageAttributeValue { DataType = "String", StringValue = message.Service } },
+                    { "warning", new MessageAttributeValue { DataType = "String", StringValue = message.Warning } }
+                }
+            };
+
+            await _amazonSimpleNotificationService.PublishAsync(request);
+        }
+
+        public void Dispose()
+        {
+            _amazonSimpleNotificationService.Dispose();
+        }
+    }
+}
