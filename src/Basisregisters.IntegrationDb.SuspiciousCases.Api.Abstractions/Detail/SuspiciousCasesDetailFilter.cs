dd5644dd0ef6b7fb206a97d78545822f716462f5
emalfroy <egon.malfroy@gmail.com>
2024-01-25 09:16:41 +0100

    fix: move supsicious cases filters to abstractions

---
+++ src/Basisregisters.IntegrationDb.SuspiciousCases.Api.Abstractions/Detail/SuspiciousCasesDetailFilter.cs
@@ -0,0 +1,7 @@
+namespace Basisregisters.IntegrationDb.SuspiciousCases.Api.Abstractions.Detail
+{
+    public class SuspiciousCasesDetailFilter
+    {
+        public string? NisCode { get; set; }
+    }
+}
---
+++ src/Basisregisters.IntegrationDb.SuspiciousCases.Api.Abstractions/List/SuspiciousCasesListFilter.cs
@@ -0,0 +1,7 @@
+namespace Basisregisters.IntegrationDb.SuspiciousCases.Api.Abstractions.List
+{
+    public class SuspiciousCasesListFilter
+    {
+        public string? NisCode { get; set; }
+    }
+}
