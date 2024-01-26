f80587b864ce181093753d5c3559a8328d878b56
Arne Dumarey <dumarey.arne@gmail.com>
2024-01-26 15:51:34 +0100

    feat: add nationalregistry skeleton

---
+++ src/Basisregisters.IntegrationDb.NationalRegistry/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Basisregisters.IntegrationDb.NationalRegistry.Model;
+using FlatFiles;
+using FlatFiles.TypeMapping;
+
+Console.WriteLine("Hello, let's process national registry data!");
+
+using (TextReader textReader = new StringReader("11001263010100002    ADRIAAN SANDERSLEI              0000\n11001263010300036B001ANTWERPSESTEENWEG               0000"))
+{
+    var mapper = FlatFileRecord.Mapper;
+    var records = mapper.Read(textReader).ToList();
+
+    Console.WriteLine(records.Count);
+}
+
+Environment.Exit(0);
