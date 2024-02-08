21c5f546230b14004cb62f0d34c57c1d1510a9db
emalfroy <egon.malfroy@gmail.com>
2024-02-08 14:05:47 +0100

    feat: add view AddressesLinkedToMultipleBuildingUnits

--- src/Basisregisters.IntegrationDb.SuspiciousCases/Views/AddressesLinkedToMultipleBuildingUnits.cs
+++ src/Basisregisters.IntegrationDb.SuspiciousCases/Views/AddressesLinkedToMultipleBuildingUnits.cs
@@ -1,57 +1,60 @@
-﻿// namespace Basisregisters.IntegrationDb.Schema.Views.SuspiciousCases
-// {
-//     using System;
-//     using Microsoft.EntityFrameworkCore;
-//     using Microsoft.EntityFrameworkCore.Metadata.Builders;
-//
-//     public class AddressesLinkedToMultipleBuildingUnits
-//     {
-//         public int AddressPersistentLocalId { get; set; }
-//         public int LinkedBuildingUnitCount { get; set; }
-//         public int NisCode { get; set; }
-//         public DateTimeOffset Timestamp { get; set; }
-//
-//         public AddressesLinkedToMultipleBuildingUnits()
-//         { }
-//     }
-//
-//     public sealed class AddressesLinkedToMultipleBuildingUnitsConfiguration : IEntityTypeConfiguration<AddressesLinkedToMultipleBuildingUnits>
-//     {
-//         public void Configure(EntityTypeBuilder<AddressesLinkedToMultipleBuildingUnits> builder)
-//         {
-//             builder
-//                 .ToView(nameof(AddressesLinkedToMultipleBuildingUnits), SuspiciousCasesContext.Schema)
-//                 .HasNoKey()
-//                 .ToSqlQuery(@$"
-//                             SELECT
-//                                 ""AddressPersistentLocalId"",
-//                                 ""LinkedBuildingUnitCount"",
-//                                 ""NisCode"",
-//                                 ""Timestamp""
-//                             FROM {ViewName} ");
-//         }
-//
-//
-//         public const string ViewName = @$"""{SuspiciousCasesContext.Schema}"".""VIEW_{nameof(AddressesLinkedToMultipleBuildingUnits)}""";
-//
-//         public const string Create = $@"
-//             CREATE MATERIALIZED VIEW IF NOT EXISTS {ViewName} AS
-//             SELECT
-//                 a.""PersistentLocalId"" AS ""AddressPersistentLocalId"",
-//                 COUNT(*) AS ""LinkedBuildingUnitCount"",
-//                 a.""NisCode"",
-//                 CURRENT_TIMESTAMP AS ""Timestamp""
-//
-//             FROM {BuildingUnitAddressRelationConfiguration.ViewName} relation
-//             JOIN ""Integration"".""Addresses"" a ON a.""PersistentLocalId"" = relation.""AddressPersistentLocalId""
-//             WHERE
-//                 a.""IsRemoved"" = false
-//             GROUP BY
-//                 a.""PersistentLocalId""
-//             HAVING
-//                 COUNT(*) > 1;
-//
-//             CREATE INDEX ""IX_{nameof(AddressesLinkedToMultipleBuildingUnits)}_AddressPersistentLocalId"" ON {ViewName} USING btree (""{nameof(AddressesLinkedToMultipleBuildingUnits.AddressPersistentLocalId)}"");
-//         ";
-//     }
-// }
+﻿namespace Basisregisters.IntegrationDb.SuspiciousCases.Views
+{
+    using Infrastructure;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class AddressesLinkedToMultipleBuildingUnits : SuspiciousCase
+    {
+        public int AddressPersistentLocalId { get; set; }
+        public override Category Category => Category.Address;
+    }
+
+    public sealed class AddressesLinkedToMultipleBuildingUnitsConfiguration : IEntityTypeConfiguration<AddressesLinkedToMultipleBuildingUnits>
+    {
+        public void Configure(EntityTypeBuilder<AddressesLinkedToMultipleBuildingUnits> builder)
+        {
+            builder
+                .ToView(ViewName, Schema.SuspiciousCases)
+                .HasNoKey()
+                .ToSqlQuery(@$"
+                            SELECT
+                                persistent_local_id,
+                                nis_code,
+                                description
+                            FROM  {Schema.SuspiciousCases}.{ViewName}");
+
+            builder.Property(x => x.AddressPersistentLocalId).HasColumnName("persistent_local_id");
+            builder.Property(x => x.NisCode).HasColumnName("nis_code");
+            builder.Property(x => x.Description).HasColumnName("description");
+        }
+
+
+        public const string ViewName = "addresses_linked_to_multiple_building_units";
+
+        // TODO: description
+        public const string Create = $@"
+            CREATE VIEW {Schema.SuspiciousCases}.{ViewName} AS
+
+            SELECT
+                c.address_persistent_local_id,
+                b.nis_code
+            FROM {SchemaLatestItems.Building} b
+            LEFT OUTER JOIN {SchemaLatestItems.BuildingUnit} bu ON bu.building_persistent_local_id = b.building_persistent_local_id
+            LEFT OUTER JOIN {SchemaLatestItems.BuildingUnitAddresses} rel ON rel.building_unit_persistent_local_id = bu.building_unit_persistent_local_id
+            JOIN (
+            SELECT
+                rel.address_persistent_local_id,
+                COUNT(*) AS linked_building_units
+            FROM {SchemaLatestItems.BuildingUnitAddresses} rel
+            LEFT OUTER JOIN {SchemaLatestItems.Address} a ON a.persistent_local_id = rel.address_persistent_local_id
+            WHERE
+                a.removed = false
+            AND
+                a.status IN (1,2)
+            GROUP BY
+                rel.address_persistent_local_id
+            HAVING
+                COUNT(*) > 1) c ON c.address_persistent_local_id = rel.address_persistent_local_id";
+    }
+}
