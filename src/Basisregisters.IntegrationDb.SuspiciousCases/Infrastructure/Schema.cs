82abb7959ab938cfc582d65837dae9cd9e1cf811
emalfroy <egon.malfroy@gmail.com>
2024-02-08 13:16:30 +0100

    feat: add view ActiveBuildingUnitWithoutAddress

--- src/Basisregisters.IntegrationDb.SuspiciousCases/Infrastructure/Schema.cs
+++ src/Basisregisters.IntegrationDb.SuspiciousCases/Infrastructure/Schema.cs
@@ -6,6 +6,7 @@
         public const string Address = "integration_address.address_latest_items";
         public const string Building = "integration_building.building_latest_items";
         public const string BuildingUnit = "integration_building.building_unit_latest_items";
+        public const string BuildingUnitAddresses = "integration_building.building_unit_addresses";
         public const string Parcel = "integration_parcel.parcel_latest_items";
         public const string Postal = "integration_postal.postal_latest_items";
         public const string Municipality = "integration_municipality.municipality_latest_items";
--- src/Basisregisters.IntegrationDb.SuspiciousCases/Views/ActiveBuildingUnitWithoutAddress.cs
+++ src/Basisregisters.IntegrationDb.SuspiciousCases/Views/ActiveBuildingUnitWithoutAddress.cs
@@ -1,54 +1,51 @@
-﻿// namespace Basisregisters.IntegrationDb.Schema.Views.SuspiciousCases
-// {
-//     using System;
-//     using Microsoft.EntityFrameworkCore;
-//     using Microsoft.EntityFrameworkCore.Metadata.Builders;
-//
-//     public class ActiveBuildingUnitWithoutAddress
-//     {
-//         public int BuildingUnitPersistentLocalId { get; set; }
-//         public int NisCode { get; set; }
-//         public DateTimeOffset Timestamp { get; set; }
-//
-//         public ActiveBuildingUnitWithoutAddress()
-//         { }
-//     }
-//
-//     public sealed class ActiveBuildingUnitWithoutAddressConfiguration : IEntityTypeConfiguration<ActiveBuildingUnitWithoutAddress>
-//     {
-//         public void Configure(EntityTypeBuilder<ActiveBuildingUnitWithoutAddress> builder)
-//         {
-//             builder
-//                 .ToView(nameof(ActiveBuildingUnitWithoutAddress), SuspiciousCasesContext.Schema)
-//                 .HasNoKey()
-//                 .ToSqlQuery(@$"SELECT
-//                                 ""BuildingUnitPersistentLocalId"",
-//                                 ""NisCode"",
-//                                 ""Timestamp""
-//                                 FROM  {ViewName}");
-//         }
-//
-//         public const string ViewName = @$"""{SuspiciousCasesContext.Schema}"".""VIEW_{nameof(ActiveBuildingUnitWithoutAddress)}""";
-//
-//         public const string Create = $@"
-//             CREATE MATERIALIZED VIEW IF NOT EXISTS {ViewName} AS
-//                 SELECT
-//                     bu.""PersistentLocalId"" AS ""BuildingUnitPersistentLocalId"",
-//                     muni.""NisCode"" AS ""NisCode"",
-//                     CURRENT_TIMESTAMP AS ""Timestamp""
-//                 FROM ""Integration"".""BuildingUnits"" AS bu
-//                 LEFT OUTER JOIN ""Integration"".""VIEW_BuildingUnitAddressRelations"" AS rel
-//                     ON bu.""PersistentLocalId"" = rel.""BuildingUnitPersistentLocalId""
-//                 INNER JOIN ""Integration"".""MunicipalityGeometries"" AS muni
-//                     ON ST_Within(bu.""Geometry"", muni.""Geometry"") IS TRUE
-//                 WHERE
-//                     bu.""IsRemoved"" IS FALSE
-//                     AND bu.""Status"" IN ('gepland','gerealiseerd')
-//                     AND rel.""AddressPersistentLocalId"" IS NULL
-//                 ORDER BY bu.""PersistentLocalId"";
-//
-//             CREATE INDEX ""IX_BuildingUnitPersistentLocalId"" ON {ViewName} USING btree (""{nameof(ActiveBuildingUnitWithoutAddress.BuildingUnitPersistentLocalId)}"");
-//             CREATE INDEX ""IX_NisCode"" ON {ViewName} USING btree (""{nameof(ActiveBuildingUnitWithoutAddress.NisCode)}"");
-//             ";
-//     }
-// }
+﻿namespace Basisregisters.IntegrationDb.SuspiciousCases.Views
+{
+    using Infrastructure;
+    using SuspiciousCases;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class ActiveBuildingUnitWithoutAddress : SuspiciousCase
+    {
+        public int BuildingUnitPersistentLocalId { get; set; }
+        public override Category Category => Category.BuildingUnit;
+    }
+
+    public sealed class ActiveBuildingUnitWithoutAddressConfiguration : IEntityTypeConfiguration<ActiveBuildingUnitWithoutAddress>
+    {
+        public void Configure(EntityTypeBuilder<ActiveBuildingUnitWithoutAddress> builder)
+        {
+            builder
+                .ToView(ViewName, Schema.SuspiciousCases)
+                .HasNoKey()
+                .ToSqlQuery(@$"
+                            SELECT
+                                building_unit_persistent_local_id,
+                                nis_code,
+                                description
+                            FROM  {Schema.SuspiciousCases}.{ViewName}");
+
+            builder.Property(x => x.BuildingUnitPersistentLocalId).HasColumnName("building_unit_persistent_local_id");
+            builder.Property(x => x.NisCode).HasColumnName("nis_code");
+            builder.Property(x => x.Description).HasColumnName("description");
+        }
+
+        public const string ViewName = "view_active_building_unit_without_address";
+
+        public const string Create = $@"
+            CREATE VIEW {Schema.SuspiciousCases}.{ViewName} AS
+                SELECT
+                    bu.building_unit_persistent_local_id,
+                    b.nis_code
+                FROM {SchemaLatestItems.BuildingUnit} AS bu
+                LEFT OUTER JOIN {SchemaLatestItems.BuildingUnitAddresses} AS rel
+                    ON bu.building_unit_persistent_local_id = rel.building_unit_persistent_local_id
+                LEFT OUTER JOIN {SchemaLatestItems.Building} AS b
+                    ON bu.building_persistent_local_id = bu.building_persistent_local_id
+                WHERE
+                    bu.is_removed IS FALSE
+                    AND rel.address_persistent_local_id IS NULL
+                ORDER BY bu.building_unit_persistent_local_id;
+            ";
+    }
+}
