0b5253c154b4ff6a8b1237dd0dfde28ef356752e
emalfroy <egon.malfroy@gmail.com>
2024-02-09 15:59:44 +0100

    feat: add view CurrentAddressWithoutLinkedParcelOrBuildingUnit

--- src/Basisregisters.IntegrationDb.SuspiciousCases/Infrastructure/Schema.cs
+++ src/Basisregisters.IntegrationDb.SuspiciousCases/Infrastructure/Schema.cs
@@ -8,6 +8,7 @@
         public const string BuildingUnit = "integration_building.building_unit_latest_items";
         public const string BuildingUnitAddresses = "integration_building.building_unit_addresses";
         public const string Parcel = "integration_parcel.parcel_latest_items";
+        public const string ParcelAddresses = "integration_parcel.parcel_latest_item_addresses";
         public const string Postal = "integration_postal.postal_latest_items";
         public const string Municipality = "integration_municipality.municipality_latest_items";
         public const string MunicipalityGeometries = "integration_municipality.municipality_geometries";
