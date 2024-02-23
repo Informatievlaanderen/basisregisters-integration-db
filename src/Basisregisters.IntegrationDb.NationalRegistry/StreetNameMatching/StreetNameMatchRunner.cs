e39121d499e7abb88edd2bc2effd069e99e1eed4
jvandaal <jonas.vandaal@outlook.be>
2024-02-23 11:53:39 +0100

    chore: streetname matching investigation

--- src/Basisregisters.IntegrationDb.NationalRegistry/StreetNameMatching/StreetNameMatchRunner.cs
+++ src/Basisregisters.IntegrationDb.NationalRegistry/StreetNameMatching/StreetNameMatchRunner.cs
@@ -3,6 +3,7 @@
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
+    using System.Text.RegularExpressions;
     using Repositories;
 
     public class StreetNameMatchRunner
@@ -22,7 +23,7 @@
                     x => x.Key,
                     x => x.Select(y => y.StreetName).ToList());
 
-            var matched = new Dictionary<string, List<string>>();
+            var matched = new Dictionary<string, List<StreetNameMatches>>();
             var unmatched = new Dictionary<string, List<string>>();
 
             foreach (var (nisCode, streetNames) in streetNamesByNisCode)
@@ -30,14 +31,14 @@
                 var dbStreetNames = _repo.GetStreetNamesByNisCode(nisCode);
 
                 var matcher = new StreetNameMatcher(dbStreetNames);
-                matched.Add(nisCode, new List<string>());
+                matched.Add(nisCode, new List<StreetNameMatches>());
                 unmatched.Add(nisCode, new List<string>());
                 foreach (var streetName in streetNames)
                 {
-                    var match = matcher.MatchStreetName(nisCode, streetName);
+                    var match = matcher.MatchStreetName(nisCode, streetName).ToList();
                     if (match.Any())
                     {
-                        matched[nisCode].Add(streetName);
+                        matched[nisCode].Add(new StreetNameMatches(nisCode, streetName, match));
                     }
                     else
                     {
@@ -54,11 +55,57 @@
                 }
             }
 
-            File.WriteAllLines(@"C:\DV\inwonersaantallen\unmatched.csv",
-                unmatched
-                    .SelectMany(x => x.Value.Select(y => new { NisCode = x.Key, StreetName = y }))
-                    .Select(x => $"{x.NisCode};{x.StreetName}")
+            // var matchesWithDifferentNaming = new List<StreetNameMatches>();
+            var result = new List<(string NisCode, StreetName StreetName, List<string> Searches)>();
+            foreach (var nisCode in matched)
+            {
+                // Probleem 1: heeft een zoekresultaat meerdere matches met andere schrijfwijze
+                // foreach (var match in nisCode.Value.Where(x => x.HasDifferentNaming))
+                // {
+                //     matchesWithDifferentNaming.Add(match);
+                // }
+
+                // Probleem 2: is een database straatnaam gematched met meerdere zoek straatnamen waarbij het niet gaat over een homoniemtoevoeging     .ToList();
+                foreach (var streetName in nisCode.Value.SelectMany(x => x.Matched).Distinct())
+                {
+                    var occurances = nisCode.Value
+                        .Where(x => x.Matched.Contains(streetName))
+                        .ToList();
+
+                    if (occurances.Count > 1)
+                    {
+                        result.Add((nisCode.Key, streetName, occurances.Select(x => x.Search).ToList()));
+                    }
+                }
+            }
+
+            File.WriteAllLines(@"C:\DV\inwonersaantallen\doubles.csv",
+                result
+                    .Select(x => $"{x.NisCode};{x.StreetName.NameDutch};{x.StreetName.HomonymAdditionDutch};{x.Searches.Aggregate((i,j) => $"{i},{j}")}")
                     .Distinct());
+
+            // File.WriteAllLines(@"C:\DV\inwonersaantallen\unmatched.csv",
+            //     unmatched
+            //         .SelectMany(x => x.Value.Select(y => new { NisCode = x.Key, StreetName = y }))
+            //         .Select(x => $"{x.NisCode};{x.StreetName}")
+            //         .Distinct());
         }
+    }
+
+    public sealed record StreetNameMatches(
+        string NisCode,
+        string Search,
+        List<StreetName> Matched)
+    {
+        public bool HasDifferentNaming => Matched
+            .GroupBy(x => x.NameDutch)
+            .Count() > 1;
+
+        public bool HasHomonymAdditionFound => Matched
+            .Any(x =>
+                !string.IsNullOrWhiteSpace(x.HomonymAdditionDutch)
+                || !string.IsNullOrWhiteSpace(x.HomonymAdditionFrench)
+                || !string.IsNullOrWhiteSpace(x.HomonymAdditionGerman)
+                || !string.IsNullOrWhiteSpace(x.HomonymAdditionEnglish));
     }
 }
