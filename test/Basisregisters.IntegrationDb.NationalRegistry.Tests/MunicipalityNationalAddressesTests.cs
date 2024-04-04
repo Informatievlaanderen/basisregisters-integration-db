f4de84299fe1ba56b6d3444bee9b1370602568d8
Rik De Peuter <rikdepeuter@gmail.com>
2024-04-04 15:25:05 +0200

    add Halen tests

--- test/Basisregisters.IntegrationDb.NationalRegistry.Tests/MunicipalityNationalAddressesTests.cs
+++ test/Basisregisters.IntegrationDb.NationalRegistry.Tests/MunicipalityNationalAddressesTests.cs
@@ -563,6 +563,33 @@ namespace Basisregisters.IntegrationDb.NationalRegistry.Tests
         }
 
         [Theory]
+        [InlineData("0046", "B002", "46", "B002")]
+        [InlineData("0046", "A1.1", "46", "A1.1")]
+        [InlineData("0046", "W001", "46", "W1")]
+        [InlineData("0046", "W012", "46", "W012")]
+        [InlineData("0046", "AW01", "46A", "W1")]
+        [InlineData("0046", "BW01", "46B", "W1")]
+        [InlineData("0046", "A000", "46A", null)]
+        [InlineData("0046", "G/02", "46G", "02")]
+        [InlineData("0046", "G/2 ", "46G", "2")]
+        public void Halen(string houseNumber, string index, string expectedHouseNumber, string? expectedBoxNumber)
+        {
+            var record = new FlatFileRecord
+            {
+                NisCode = "71020",
+                HouseNumber = houseNumber,
+                Index = new NationalRegistryIndex(index)
+            };
+            var address = new NationalRegistryAddress(record);
+
+            var result = address.HouseNumberBoxNumbers.SelectMany(x => x.GetValues()).ToList();
+
+            result.Should().NotBeEmpty();
+            result.First().HouseNumber.Should().Be(expectedHouseNumber);
+            result.First().BoxNumber.Should().Be(expectedBoxNumber);
+        }
+
+        [Theory]
         [InlineData("0046", "00OB", "46", "00OB")]
         [InlineData("0046", "001A", "46", "1A")]
         [InlineData("0046", "E000", "46E", null)]
