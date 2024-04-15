ed7ffad1fbb5d56cb6778ada11d67ec73a2e8e92
jvandaal <jonas.vandaal@outlook.be>
2024-04-15 09:27:22 +0200

    chore: add bosa download skeleton

---
+++ src/Basisregisters.IntegrationDb.Bosa/Infrastructure/Program.cs
@@ -0,0 +1,169 @@
+﻿namespace Basisregisters.IntegrationDB.Bosa.Infrastructure
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+    using Autofac;
+    using Autofac.Extensions.DependencyInjection;
+    using Be.Vlaanderen.Basisregisters.Aws.DistributedMutex;
+    using Destructurama;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+    using Serilog;
+    using Serilog.Debugging;
+    using Serilog.Extensions.Logging;
+
+    public sealed class Program
+    {
+        protected Program()
+        { }
+
+        public static async Task Main(string[] args)
+        {
+            AppDomain.CurrentDomain.FirstChanceException += (_, eventArgs) =>
+                Log.Debug(
+                    eventArgs.Exception,
+                    "FirstChanceException event raised in {AppDomain}.",
+                    AppDomain.CurrentDomain.FriendlyName);
+
+            AppDomain.CurrentDomain.UnhandledException += (_, eventArgs) =>
+                Log.Fatal((Exception)eventArgs.ExceptionObject, "Encountered a fatal exception, exiting program.");
+
+            Log.Information("Initializing IntegrationDb.Bosa");
+
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration((_, builder) =>
+                {
+                    builder
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+                        .AddJsonFile($"appsettings.{Environment.MachineName.ToLowerInvariant()}.json", optional: true,
+                            reloadOnChange: false)
+                        .AddEnvironmentVariables()
+                        .AddCommandLine(args);
+                })
+                .ConfigureLogging((hostContext, builder) =>
+                {
+                    SelfLog.Enable(Console.WriteLine);
+
+                    Log.Logger = new LoggerConfiguration() //NOSONAR logging configuration is safe
+                        .ReadFrom.Configuration(hostContext.Configuration)
+                        .Enrich.FromLogContext()
+                        .Enrich.WithMachineName()
+                        .Enrich.WithThreadId()
+                        .Enrich.WithEnvironmentUserName()
+                        .Destructure.JsonNetTypes()
+                        .CreateLogger();
+
+                    builder.ClearProviders();
+                    builder.AddSerilog(Log.Logger);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var loggerFactory = new SerilogLoggerFactory(Log.Logger); //NOSONAR logging configuration is safe
+
+                    // var connectionString = hostContext.Configuration.GetConnectionString("Consumer");
+                    // services
+                    //     .AddDbContextFactory<IdempotentConsumerContext>((_, options) => options
+                    //         .UseLoggerFactory(loggerFactory)
+                    //         .UseSqlServer(connectionString,
+                    //             sqlServerOptions =>
+                    //             {
+                    //                 sqlServerOptions.EnableRetryOnFailure();
+                    //                 sqlServerOptions.MigrationsHistoryTable(MigrationTables.Consumer, Schema.Consumer);
+                    //             }));
+                    // services
+                    //     .AddDbContext<ConsumerContext>((_, options) => options
+                    //         .UseLoggerFactory(loggerFactory)
+                    //         .UseSqlServer(connectionString, sqlServerOptions =>
+                    //         {
+                    //             sqlServerOptions.EnableRetryOnFailure();
+                    //             sqlServerOptions.MigrationsHistoryTable(MigrationTables.ConsumerProjections, Schema.ConsumerProjections);
+                    //         }));
+
+                    // services.ConfigureIdempotency(
+                    //     hostContext.Configuration
+                    //         .GetSection(IdempotencyConfiguration.Section)
+                    //         .Get<IdempotencyConfiguration>()
+                    //         .ConnectionString!,
+                    //     new IdempotencyMigrationsTableInfo(Schema.Import),
+                    //     new IdempotencyTableInfo(Schema.Import),
+                    //     loggerFactory);
+                })
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureContainer<ContainerBuilder>((hostContext, containerBuilder) =>
+                {
+                    var services = new ServiceCollection();
+                    var loggerFactory = new SerilogLoggerFactory(Log.Logger); //NOSONAR logging configuration is safe
+
+                    // containerBuilder.RegisterModule(new ApiModule(hostContext.Configuration, services, loggerFactory));
+
+                    // containerBuilder
+                    //     .RegisterType<ConsumerProjections>()
+                    //     .As<IHostedService>()
+                    //     .SingleInstance();
+
+                    containerBuilder.Populate(services);
+                })
+                .UseConsoleLifetime()
+                .Build();
+
+            Log.Information("Starting IntegrationDb.Bosa");
+
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+            var loggerFactory = host.Services.GetRequiredService<ILoggerFactory>();
+            var configuration = host.Services.GetRequiredService<IConfiguration>();
+
+            try
+            {
+                await DistributedLock<Program>.RunAsync(
+                        async () =>
+                        {
+                            await host.RunAsync().ConfigureAwait(false);
+                        },
+                        DistributedLockOptions.LoadFromConfiguration(configuration),
+                        logger)
+                    .ConfigureAwait(false);
+            }
+            catch (AggregateException aggregateException)
+            {
+                foreach (var innerException in aggregateException.InnerExceptions)
+                {
+                    logger.LogCritical(innerException, "Encountered a fatal exception, exiting program.");
+                }
+            }
+            catch (Exception e)
+            {
+                logger.LogCritical(e, "Encountered a fatal exception, exiting program.");
+                Log.CloseAndFlush();
+
+                // Allow some time for flushing before shutdown.
+                await Task.Delay(500, default);
+                throw;
+            }
+            finally
+            {
+                logger.LogInformation("Stopping...");
+            }
+        }
+
+        // private static IServiceProvider ConfigureServices(IConfiguration configuration)
+        // {
+        //     var services = new ServiceCollection();
+        //     var builder = new ContainerBuilder();
+        //
+        //     var tempProvider = services.BuildServiceProvider();
+        //     var loggerFactory = tempProvider.GetRequiredService<ILoggerFactory>();
+        //
+        //     // builder.RegisterModule(new ApiModule(configuration, services, loggerFactory));
+        //     // builder.RegisterModule(new ConsumerModule(configuration, services, loggerFactory));
+        //     // builder.RegisterModule(new ProjectorModule(configuration));
+        //
+        //     builder.Populate(services);
+        //
+        //     return new AutofacServiceProvider(builder.Build());
+        // }
+    }
+}
