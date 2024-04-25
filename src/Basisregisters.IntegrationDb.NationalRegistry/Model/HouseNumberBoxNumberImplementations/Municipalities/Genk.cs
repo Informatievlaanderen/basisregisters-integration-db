bf4327787adbe57acb994e0b56e61049a22d907e
Rik De Peuter <rikdepeuter@gmail.com>
2024-04-25 15:27:17 +0200

    add Genk

---
+++ src/Basisregisters.IntegrationDb.NationalRegistry/Model/HouseNumberBoxNumberImplementations/Municipalities/Genk.cs
@@ -0,0 +1,24 @@
+namespace Basisregisters.IntegrationDb.NationalRegistry.Model.HouseNumberBoxNumberImplementations.Municipalities
+{
+    using System.Collections.Generic;
+
+    public class Genk : MunicipalityHouseNumberBoxNumbersBase
+    {
+        public Genk(string nisCode, string sourceHouseNumber, NationalRegistryIndex index) : base(nisCode, sourceHouseNumber, index)
+        { }
+
+        public override bool IsMatch() =>
+            NisCode == "71016";
+
+        public override IList<HouseNumberWithBoxNumber> GetValues()
+        {
+            return new[]
+            {
+                new HouseNumberWithBoxNumber(
+                    HouseNumberSourceValue,
+                    IndexSourceValue.Trim()
+                )
+            };
+        }
+    }
+}
