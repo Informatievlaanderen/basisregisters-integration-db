b335022aeb45006fd99e3b3133282da0a9bf0268
jvandaal <jonas.vandaal@outlook.be>
2024-07-01 15:48:18 +0200

    fix: use unique GUID in assembly info

--- src/Basisregisters.IntegrationDb.Gtmf.Meldingen/Properties/AssemblyInfo.cs
+++ src/Basisregisters.IntegrationDb.Gtmf.Meldingen/Properties/AssemblyInfo.cs
@@ -7,4 +7,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("GTMF Integration GTMF meldingen")]
 [assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
 [assembly: AssemblyCompany("Vlaamse overheid")]
-[assembly: Guid("8d362732-7362-4a26-8c34-9266116e0131")]
+[assembly: Guid("d1cf70ab-2b2d-4f47-8f97-af247073ed3d")]
