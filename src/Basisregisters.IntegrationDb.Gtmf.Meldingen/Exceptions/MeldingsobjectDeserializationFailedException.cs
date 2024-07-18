8002f035a3f2d583debfa6ed44b58eee23172b6a
jvandaal <jonas.vandaal@outlook.be>
2024-07-18 14:50:53 +0200

    fix: deserializing meldingsobject event with new line character

---
+++ src/Basisregisters.IntegrationDb.Gtmf.Meldingen/Exceptions/MeldingsobjectDeserializationFailedException.cs
@@ -0,0 +1,20 @@
+﻿namespace Basisregisters.IntegrationDb.Gtmf.Meldingen.Exceptions
+{
+    using System;
+
+    public class MeldingsobjectDeserializationFailedException : Exception
+    {
+        public int Position { get; set; }
+        public string Json { get; set; }
+
+        public MeldingsobjectDeserializationFailedException(
+            int position,
+            string json,
+            Exception innerException)
+            : base($"Failed to deserialize meldingsobject event at position {position}", innerException)
+        {
+            Position = position;
+            Json = json;
+        }
+    }
+}
