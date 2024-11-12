74de3bb98c05f890243c6b644b99b4ec6dafeb91
Arne Dumarey <dumarey.arne@gmail.com>
2024-11-12 09:31:57 +0100

    fix(suspicious): recreate parcel related views to v2 tables

--- src/Basisregisters.IntegrationDb.SuspiciousCases/Migrations/20241112080757_RecreateParcelViews.Designer.cs
+++ src/Basisregisters.IntegrationDb.SuspiciousCases/Migrations/20241112080757_RecreateParcelViews.Designer.cs
@@ -2,6 +2,7 @@
 using Basisregisters.IntegrationDb.SuspiciousCases;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Basisregisters.IntegrationDb.SuspiciousCases.Migrations
 {
     [DbContext(typeof(SuspiciousCasesContext))]
-    partial class SuspiciousCasesContextModelSnapshot : ModelSnapshot
+    [Migration("20241112080757_RecreateParcelViews")]
+    partial class RecreateParcelViews
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
