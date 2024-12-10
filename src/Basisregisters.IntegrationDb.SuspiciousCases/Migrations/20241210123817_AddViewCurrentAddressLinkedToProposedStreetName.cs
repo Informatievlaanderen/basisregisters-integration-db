cc6f9bd9171d64667ef822efde44c947971f7519
Rik De Peuter <rikdepeuter@gmail.com>
2024-12-10 13:44:58 +0100

    feat: add suspicious case CurrentAddressLinkedToProposedStreetName

---
+++ src/Basisregisters.IntegrationDb.SuspiciousCases/Migrations/20241210123817_AddViewCurrentAddressLinkedToProposedStreetName.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Basisregisters.IntegrationDb.SuspiciousCases.Migrations
+{
+    using System;
+    using Infrastructure;
+    using Views;
+
+    /// <inheritdoc />
+    public partial class AddViewCurrentAddressLinkedToProposedStreetName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"DROP VIEW IF EXISTS {Schema.SuspiciousCases}.{CurrentAddressLinkedToProposedStreetNameConfiguration.ViewName};");
+
+            migrationBuilder.Sql(CurrentAddressLinkedToProposedStreetNameConfiguration.Create);
+
+            migrationBuilder.Sql(SuspiciousCaseCountConfiguration.Create);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ src/Basisregisters.IntegrationDb.SuspiciousCases/Views/CurrentAddressLinkedToProposedStreetName.cs
@@ -0,0 +1,52 @@
+﻿namespace Basisregisters.IntegrationDb.SuspiciousCases.Views
+{
+    using Infrastructure;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public sealed class CurrentAddressLinkedToProposedStreetName : SuspiciousCase
+    {
+        public int AddressPersistentLocalId { get; set; }
+
+        public override Category Category => Category.Address;
+    }
+
+    public sealed class CurrentAddressLinkedToProposedStreetNameConfiguration : IEntityTypeConfiguration<CurrentAddressLinkedToProposedStreetName>
+    {
+        public void Configure(EntityTypeBuilder<CurrentAddressLinkedToProposedStreetName> builder)
+        {
+            builder
+                .ToView(ViewName, Schema.SuspiciousCases)
+                .HasNoKey()
+                .ToSqlQuery(@$"
+                            SELECT
+                                persistent_local_id,
+                                address_persistent_local_id,
+                                nis_code,
+                                description
+                            FROM  {Schema.SuspiciousCases}.{ViewName}");
+
+            builder.Property(x => x.PersistentLocalId).HasColumnName("persistent_local_id");
+            builder.Property(x => x.AddressPersistentLocalId).HasColumnName("address_persistent_local_id");
+            builder.Property(x => x.NisCode).HasColumnName("nis_code");
+            builder.Property(x => x.Description).HasColumnName("description");
+        }
+
+        public const string ViewName = "view_current_address_linked_to_proposed_streetname";
+
+        public const string Create = $@"
+            CREATE OR REPLACE VIEW {Schema.SuspiciousCases}.{ViewName} AS
+	        SELECT
+		        CAST(a.persistent_local_id as varchar) AS persistent_local_id,
+		        a.persistent_local_id AS address_persistent_local_id,
+		        s.nis_code AS nis_code,
+                {Schema.FullAddress}(s.name_dutch, a.house_number, a.box_number, a.postal_code, m.name_dutch) as description
+	        FROM {SchemaLatestItems.Address} AS a
+            JOIN {SchemaLatestItems.StreetName} s ON s.persistent_local_id = a.street_name_persistent_local_id AND s.status = 0
+            JOIN {SchemaLatestItems.Municipality} m ON s.municipality_id = m.municipality_id
+	        WHERE
+                a.status = 2
+	            AND a.removed = false
+            ;";
+    }
+}
