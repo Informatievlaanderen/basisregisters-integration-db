52eeebba1c28cb88d078a1af2a9b4893a7f76d2b
Arne Dumarey <dumarey.arne@gmail.com>
2025-01-09 14:47:41 +0100

    fix(nationalregistry): correct niscode hardcoded merger municipality

--- src/Basisregisters.IntegrationDb.NationalRegistry/StreetNameMatching/HardCodedStreetNames.cs
+++ src/Basisregisters.IntegrationDb.NationalRegistry/StreetNameMatching/HardCodedStreetNames.cs
@@ -10,8 +10,8 @@
             new HardCodedStreetName("23002", "DOORNVELD", "Z. 3 Doornveld"),
             new HardCodedStreetName("23081", "TRANSITCENTRUM 127-HAACHTSESTWG", "Haachtsesteenweg"),
             new HardCodedStreetName("34022", "MARCONILAAN(KOR)", "Guglielmo Marconilaan"),
-            new HardCodedStreetName("71022", "ABDIS DE GOOR", "Abdis de Goorstraat"),
-            new HardCodedStreetName("71022", "ABDIS DE MOMBEEK", "Abdis de Mombeekstraat"),
+            new HardCodedStreetName("71072", "ABDIS DE GOOR", "Abdis de Goorstraat"),
+            new HardCodedStreetName("71072", "ABDIS DE MOMBEEK", "Abdis de Mombeekstraat"),
             new HardCodedStreetName("73066", "HOLSTRAAT-HEUKELOM", "Holstraat"),
             new HardCodedStreetName("34022", "STEYTS KOER(KOR)(NIEUWE STRAAT)", "Steyts Koer")
         };
