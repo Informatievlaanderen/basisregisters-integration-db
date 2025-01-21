9eb813dac6b03ed3a10dda47c05e4f56fd18e30e
jvandaal <jonas.vandaal@outlook.be>
2025-01-21 20:39:30 +0100

    refactor(gtmf): rename to initiator organisatie

---
+++ src/Basisregisters.IntegrationDb.Gtmf.Meldingen/Migrations/20250121193845_RenameToInitiatorOrganisatie.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Basisregisters.IntegrationDb.Gtmf.Meldingen.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameToInitiatorOrganisatie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "organisatie_id_internal",
+                schema: "integration_gtmf",
+                table: "meldingsobject_statuswijziging",
+                newName: "initiator_organisatie_id_internal");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_meldingsobject_statuswijziging_organisatie_id_internal",
+                schema: "integration_gtmf",
+                table: "meldingsobject_statuswijziging",
+                newName: "IX_meldingsobject_statuswijziging_initiator_organisatie_id_int~");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "initiator_organisatie_id_internal",
+                schema: "integration_gtmf",
+                table: "meldingsobject_statuswijziging",
+                newName: "organisatie_id_internal");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_meldingsobject_statuswijziging_initiator_organisatie_id_int~",
+                schema: "integration_gtmf",
+                table: "meldingsobject_statuswijziging",
+                newName: "IX_meldingsobject_statuswijziging_organisatie_id_internal");
+        }
+    }
+}
