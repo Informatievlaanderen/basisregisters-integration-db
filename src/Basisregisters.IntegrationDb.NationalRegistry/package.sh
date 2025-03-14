f7f5c00c7478279b36d2f8323d27219b2b329522
ArneD <dumarey.arne@gmail.com>
2025-03-14 09:34:28 +0100

    chore(nationalregistry): package also on linux

--- src/Basisregisters.IntegrationDb.NationalRegistry/package.sh
+++ src/Basisregisters.IntegrationDb.NationalRegistry/package.sh
@@ -5,4 +5,10 @@ dotnet restore ../..
 dotnet build --no-incremental --no-restore -c Release --self-contained
 dotnet publish -o dist --no-build --no-restore -c Release -p:PublishReadyToRun=True --self-contained
 rm dist/appsettings.*.json 
-powershell -Command "Compress-Archive -Path ./dist/* -DestinationPath output.zip -Force"
+
+# Check if we're on Linux or Windows and use appropriate compression command
+if [ "$(uname)" = "Linux" ]; then
+    zip -r output.zip dist/*
+else
+    powershell -Command "Compress-Archive -Path ./dist/* -DestinationPath output.zip -Force"
+fi
