39eb890bba6e364dd9922b371ee4d020edf5b45e
Arne Dumarey <dumarey.arne@gmail.com>
2025-05-28 15:57:42 +0200

    fix(bosa): change lifespan to zulutime

--- src/Basisregisters.IntegrationDb.Bosa/BaseRegistryService.cs
+++ src/Basisregisters.IntegrationDb.Bosa/BaseRegistryService.cs
@@ -1,11 +1,16 @@
 namespace Basisregisters.IntegrationDb.Bosa
 {
     using System;
+    using Be.Vlaanderen.Basisregisters.GrAr.Extracts;
     using Extensions;
 
     public class BaseRegistryService
     {
         protected static string GetVersionAsString(DateTimeOffset value) => value.ToBelgianString();
+        protected static string GetZuluVersionAsString(DateTimeOffset value) => value
+            .ToUniversalTime()
+            .ToString("yyyy-MM-ddTHH:mm:ssZ", System.Globalization.CultureInfo.InvariantCulture);
+
         protected static string FormatNamespace(string @namespace) => @namespace.EndsWith('/') ? @namespace : $"{@namespace}/";
     }
 }
