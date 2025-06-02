e41d958bad0136d2604e7b8eb95d67e261828317
Rik De Peuter <rikdepeuter@gmail.com>
2025-06-02 15:43:33 +0200

    fix: suspicious cases reporting use dutch translations

--- src/Basisregisters.IntegrationDb.SuspiciousCases/Category.cs
+++ src/Basisregisters.IntegrationDb.SuspiciousCases/Category.cs
@@ -1,5 +1,7 @@
 ﻿namespace Basisregisters.IntegrationDb.SuspiciousCases
 {
+    using System;
+
     public enum Category
     {
         Address = 1,
@@ -8,5 +10,22 @@
         Building = 4,
         BuildingUnit = 5,
         StreetName = 6
+    }
+
+    public static class CategoryExtensions
+    {
+        public static string ToDutchString(this Category category)
+        {
+            return category switch
+            {
+                Category.Address => "Adres",
+                Category.Parcel => "Perceel",
+                Category.RoadSegment => "Wegsegment",
+                Category.Building => "Gebouw",
+                Category.BuildingUnit => "Gebouweenheid",
+                Category.StreetName => "Straatnaam",
+                _ => throw new ArgumentOutOfRangeException(nameof(category), category, null)
+            };
+        }
     }
 }
