8332a64655336374173080a59ff0ec0e96443c1f
Rik De Peuter <rikdepeuter@gmail.com>
2025-06-18 15:43:14 +0200

    feat: add enabled toggle to veka email sending

--- src/Basisregisters.Integration.Veka/Configuration/EmailOptions.cs
+++ src/Basisregisters.Integration.Veka/Configuration/EmailOptions.cs
@@ -3,5 +3,6 @@ namespace Basisregisters.Integration.Veka.Configuration
     public class EmailOptions
     {
         public required string SenderEmail { get; set; }
+        public required bool Enabled { get; set; } = true;
     }
 }
