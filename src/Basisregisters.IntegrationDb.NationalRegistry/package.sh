b60cfaad86149e264a680cf60fbdedf73fde7329
ArneD <dumarey.arne@gmail.com>
2025-09-04 11:05:39 +0200

    chore(nationalregistry): update package.sh

--- src/Basisregisters.IntegrationDb.NationalRegistry/package.sh
+++ src/Basisregisters.IntegrationDb.NationalRegistry/package.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-dotnet paket restore
 dotnet restore ../..
-dotnet build --no-incremental --no-restore -c Release --self-contained
-dotnet publish -o dist --no-build --no-restore -c Release -p:PublishReadyToRun=True --self-contained
-rm dist/appsettings.*.json 
+dotnet build --no-incremental --no-restore -c Release --self-contained -r win-x64
+dotnet publish -o dist --no-build --no-restore -c Release -p:PublishReadyToRun=True --self-contained -r win-x64
+rm dist/appsettings.*.json
 
 # Check if we're on Linux or Windows and use appropriate compression command
 if [ "$(uname)" = "Linux" ]; then
