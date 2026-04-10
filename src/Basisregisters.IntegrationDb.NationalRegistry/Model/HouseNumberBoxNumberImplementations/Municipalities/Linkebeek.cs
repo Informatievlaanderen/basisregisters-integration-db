56247976fad273c992477da1a73c01309effca25
Arne Dumarey <dumarey.arne@gmail.com>
2026-04-10 13:25:29 +0200

    feat(natreg): add Linkebeek cases

---
+++ src/Basisregisters.IntegrationDb.NationalRegistry/Model/HouseNumberBoxNumberImplementations/Municipalities/Linkebeek.cs
@@ -0,0 +1,30 @@
+namespace Basisregisters.IntegrationDb.NationalRegistry.Model.HouseNumberBoxNumberImplementations.Municipalities
+{
+    using System;
+    using System.Collections.Generic;
+
+    public sealed class Linkebeek : MunicipalityHouseNumberBoxNumbersBase
+    {
+        public Linkebeek(string nisCode, string sourceHouseNumber, NationalRegistryIndex index) : base(nisCode, sourceHouseNumber, index)
+        { }
+
+        public override bool IsMatch() => NisCode == "23100" && IndexSourceValue.Length == 4 &&
+                                          IndexSourceValue[0].ToString().ToUpper() == "B" && IsNumeric(IndexSourceValue[1..]);
+
+        public override IList<HouseNumberWithBoxNumber> GetValues()
+        {
+            if (IndexSourceValue[0].ToString().ToUpper() == "B" && IsNumeric(IndexSourceValue[1..]))
+            {
+                return
+                [
+                    new HouseNumberWithBoxNumber(
+                        HouseNumberSourceValue,
+                        Index.Right!.TrimStart('0')
+                    )
+                ];
+            }
+
+            throw new InvalidOperationException("Invalid use of matches");
+        }
+    }
+}
